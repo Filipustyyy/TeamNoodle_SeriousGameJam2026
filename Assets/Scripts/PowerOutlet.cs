30f997c31f0813aeed246c8994f9cb7762ede35d
Oskar Kalbarczyk <oskalbarczyk@gmail.com>
2026-04-25 14:13:19 +0200

    refactor(interact): split detect from response via IInteractable

--- Assets/Scripts/PowerOutlet.cs
+++ Assets/Scripts/PowerOutlet.cs
@@ -1,10 +1,17 @@
 using UnityEngine;
 
-public class PowerOutlet : MonoBehaviour
+public class PowerOutlet : MonoBehaviour, IInteractable
 {
     [SerializeField] private float cordLength = 5f;
 
     public float CordLength => cordLength;
+
+    public void Interact(GameObject interactor)
+    {
+        var tether = interactor.GetComponent<PlayerTether>();
+        if (tether == null) return;
+        tether.Plug(this);
+    }
 
     private void OnDrawGizmosSelected()
     {

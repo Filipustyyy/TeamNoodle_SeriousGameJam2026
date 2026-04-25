7b09b96fb397fd25bf6232f6035b6c8f2ae6ed95
jszbm <jorgenszachowicz@gmail.com>
2026-04-25 16:40:21 +0200

    feat: event cleanup on scene change

--- Assets/Scripts/Audio/AudioManager.cs
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -1,15 +1,21 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 using FMODUnity;
 using FMOD.Studio;
 
 public class AudioManager : MonoBehaviour
 {
+    private List<EventInstance> eventInstanceList;
+    
     public static AudioManager instance { get; private set; }
 
     private void Awake()
     {
         if (instance is not null) Debug.LogError("More than one audio manager in the scene");
         instance = this;
+
+        eventInstanceList = new List<EventInstance>();
     }
 
     public void PlayOneShot(EventReference sound, Vector3 worldPos)
@@ -19,7 +25,23 @@ public class AudioManager : MonoBehaviour
 
     public EventInstance CreateEventInstance(EventReference eventReference)
     {
+        
         var eventInstance = RuntimeManager.CreateInstance(eventReference);
+        eventInstanceList.Add(eventInstance);
         return eventInstance;
+    }
+
+    private void CleanUp()
+    {
+        foreach (var eventInstance in eventInstanceList)
+        {
+            eventInstance.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
+            eventInstance.release();
+        }
+    }
+
+    private void OnDestroy()
+    {
+        CleanUp();
     }
 }

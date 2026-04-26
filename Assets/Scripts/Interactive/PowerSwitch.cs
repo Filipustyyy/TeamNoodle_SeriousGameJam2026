e17614b5356b45c091fdcdbbe3758911cdb001bc
Filip Jelski <41472322+Filipustyyy@users.noreply.github.com>
2026-04-26 15:39:34 +0200

    Power switch sprite

--- Assets/Scripts/Interactive/PowerSwitch.cs
+++ Assets/Scripts/Interactive/PowerSwitch.cs
@@ -5,17 +5,26 @@ public class PowerSwitch : MonoBehaviour, IInteractable
     [Header("Connections")]
     [SerializeField] private PowerOutlet targetOutlet;
     
+    private SpriteRenderer _spriteRenderer;
+    [SerializeField] private Sprite onSprite;
+    [SerializeField] private Sprite offSprite;
+    
     private bool _isOn = false;
 
+    void Awake()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        _spriteRenderer.sprite = offSprite;
+    }
+    
     public void Interact(GameObject interactor)
     {
         if (targetOutlet != null && !_isOn)
         {
             targetOutlet.PowerOn();
+            _isOn = true;
+            _spriteRenderer.sprite = onSprite;
         }
-        else
-        {
-            Debug.LogWarning("Power Switch doesnt have a target outlet assigned!");
-        }
+        
     }
 }
